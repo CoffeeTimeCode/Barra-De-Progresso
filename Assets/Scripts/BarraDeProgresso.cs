ce80d08a9a71e2f69734b4e8b8b261ffa98f783e
Filipe Bellezoni <bellezoni@gmail.com>
2016-07-20 14:35:32 -0300

    Primeiro Commit

---
+++ Assets/Scripts/BarraDeProgresso.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BarraDeProgresso : MonoBehaviour {
+
+	public StatusBarra _statusBarra;
+
+	public GameObject _barraDeProgesso;
+	public Text _textoProgresso;
+	public float _maxProgresso;
+	public float _progressoAtual;
+
+	// Use this for initialization
+	void Start () {
+		_statusBarra = this.gameObject.GetComponent<StatusBarra> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_barraDeProgesso.transform.localScale = new Vector3 (_statusBarra.PegarTamanhoBarra(_progressoAtual,_maxProgresso),_barraDeProgesso.transform.localScale.y,_barraDeProgesso.transform.localScale.z);
+		_textoProgresso.text = _statusBarra.PegarPorcentagemBarra (_progressoAtual, _maxProgresso, 100)+"%";
+
+		if (_progressoAtual < _maxProgresso) {
+			_progressoAtual += Time.deltaTime * 5;
+		} else {
+			_textoProgresso.text = "100% Completo";	
+		}
+	}
+}
---
+++ Assets/Scripts/StatusBarra.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatusBarra : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public float PegarTamanhoBarra(float _minValor, float _maxValor){
+		return _minValor / _maxValor;
+	}
+
+	public int PegarPorcentagemBarra(float _minValor, float _maxValor, int _fator){
+		return Mathf.RoundToInt( PegarTamanhoBarra (_minValor, _maxValor) * _fator );
+	}
+}
